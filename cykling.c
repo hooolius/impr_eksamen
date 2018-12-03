421556bebe0a8b07f6ea984772bd7afb81777f27
Julius Nyborg Olesen <hoolius.o@gmail.com>
2018-12-03 09:57:41 +0100

    cleaned up some functions, added comments

--- cykling.c
+++ cykling.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <ctype.h>
 
 /* constants */
@@ -76,47 +77,49 @@ int participation_points(char position[]);
 int placement_points(char position[], int participants);
 int struct_compare(const void *a, const void *b);
 int compare_points_and_name(const void *a, const void *b);
-void print_italians_over30();
+void print_italians_over30(FILE *pinput_file);
 void calc_italians_over30();
-void print_finished_danes();
+void print_finished_danes(FILE *pinput_file);
 void find_finished_danes();
 int calc_finished_races(racer racer);
-void print_top10_finishers();
+void print_top10_finishers(FILE *pinput_file);
 void calc_top10_finishers();
-void print_paris_amstel_winner();
+void print_paris_amstel_winner(FILE *pinput_file);
 int calc_paris_amstel_winner(FILE *pinput_file, racer racers[], char time[]);
 int calc_total_time(char time1[], char time2[]);
 int time_compare(const void *a, const void *b);
 void reformat_time(int seconds, char time[]);
-void print_top10_avg_age();
+void print_top10_avg_age(FILE *pinput_file);
 double calc_top10_avg_age(FILE *pinput_file);
 
+/* The main function handles what function to call*/
 int main(int argc, char *argv[]){
+  FILE *pinput_file = fopen("cykelloeb", "r");
   int quit = 0;
   if (print_or_prompt(argc, argv)){
-    print_italians_over30();
-    print_finished_danes();
-    print_top10_finishers();
-    print_paris_amstel_winner();
-    print_top10_avg_age();
+    print_italians_over30(pinput_file);
+    print_finished_danes(pinput_file);
+    print_top10_finishers(pinput_file);
+    print_paris_amstel_winner(pinput_file);
+    print_top10_avg_age(pinput_file);
   }
   else{
     while(!quit){
       switch(prompt_for_function()){
         case 1:
-          print_italians_over30();
+          print_italians_over30(pinput_file);
           break;
         case 2:
-          print_finished_danes();
+          print_finished_danes(pinput_file);
           break;
         case 3:
-          print_top10_finishers();
+          print_top10_finishers(pinput_file);
           break;
         case 4:
-          print_paris_amstel_winner();
+          print_paris_amstel_winner(pinput_file);
           break;
         case 5:
-          print_top10_avg_age();
+          print_top10_avg_age(pinput_file);
           break;
         case -1:
           quit = 1;
@@ -127,9 +130,13 @@ int main(int argc, char *argv[]){
       }
     }
   }
+  fclose(pinput_file);
   return 0;
 }
 
+/* prompt_for_function is called when the --print option is not specified 
+ * it asks the user for a function to call, then returns the value to the 
+ * switch in main*/
 int prompt_for_function(){
   int prompt_answer = 0;
 
@@ -149,6 +156,9 @@ int prompt_for_function(){
   return prompt_answer;
 }
 
+/* simple function for testing whether or not the print argument is used, 
+ * I made it a for loop so the function would still work if more possible 
+ * arguments were added later on*/
 int print_or_prompt(int argc, char *argv[]){
   int bool_print = 0, i;
   for (i = 0; i<argc; i++){
@@ -159,6 +169,8 @@ int print_or_prompt(int argc, char *argv[]){
   return bool_print;
 }
 
+/* Simple line counting with the possibility of counting only lines 
+ * containing a particular string. Based on Kurt's simple grep */
 int line_counter(FILE *pinput_file, char *search_string){
   int n = 0;
   char line[MAX_LINES];
@@ -172,7 +184,7 @@ int line_counter(FILE *pinput_file, char *search_string){
   return n;
 }
 
-
+/* gen_racers fills up a racer struct array */
 void gen_racers(FILE *pinput_file, int lines, parsed_line *parsed_lines,
     racer racers[]){
   int i, j = -1;
@@ -198,6 +210,7 @@ void gen_racers(FILE *pinput_file, int lines, parsed_line *parsed_lines,
   } 
 }
 
+/* gen races fills the races array of a racer struct */
 void gen_races(FILE *pinput_file, parsed_line parsed_lines[],
     racer racers[], int i, int j){
   int participants_paris = line_counter(pinput_file, "ParisRoubaix");
@@ -242,26 +255,38 @@ void gen_races(FILE *pinput_file, parsed_line parsed_lines[],
     racers[j].races[(int)bastogne].participated = 1;
   }
 }
+
+/* file_parser parses the input file for valid input. If an invalid
+ * line is found, it is removed from the parsed_lines array to 
+ * prevent any bad data*/
 void file_parser(FILE *pinput_file, int lines, parsed_line parsed_lines[]){
-  int i;
+  int i, safety;
 
   for (i = 0; i < lines; i++){
-    fscanf(pinput_file, " %s \"%[' a-zA-Z -]\" | %lf %s %s | %s %[0-9:-]",
+    safety = fscanf(pinput_file, " %s \"%[' a-zA-Z -]\" |"
+        " %lf %s %s | %s %[0-9:-]",
         parsed_lines[i].race_name, parsed_lines[i].racer_name,
         &parsed_lines[i].racer_age, parsed_lines[i].team_name,
         parsed_lines[i].racer_country, parsed_lines[i].position,
         parsed_lines[i].time);
+    if (safety != 7){
+      memset((parsed_line*) parsed_lines + i, '0', sizeof(parsed_lines[0]));
+      return;
+    }
   }
   rewind(pinput_file);
   return;
 }
 
+/* sorts based on racer name */
 int struct_compare(const void *a, const void *b){
   parsed_line *ia = (parsed_line *)a;
   parsed_line *ib = (parsed_line *)b;
+
   return strcmp(ia->racer_name, ib->racer_name);
 }
 
+/* takes a full name and splits it into first and last names */
 void gen_first_and_last_name(char full_name[], char first_name[],
     char last_name[]){
   int j = strlen(full_name);
@@ -273,6 +298,8 @@ void gen_first_and_last_name(char full_name[], char first_name[],
   strcpy(last_name, pfull_name);
   strncpy(first_name, full_name, j+1);
 }
+
+/* generates the points a racer has earned in a given race */
 int gen_points(char position[], int participants){
   int points = 0;
   points += participation_points(position);
@@ -281,6 +308,7 @@ int gen_points(char position[], int participants){
   return points;
 }
 
+/* generates all points a racer has earned for participating in a race */
 int participation_points(char position[]){
   int points = 0;
   if (atoi(position) != 0){
@@ -292,6 +320,8 @@ int participation_points(char position[]){
   return points;
 }
 
+/* generates all points a racer has earned based on their finish
+ * position in a race */
 int placement_points(char position[], int participants){
   int points = 0;
   int pos;
@@ -312,6 +342,8 @@ int placement_points(char position[], int participants){
   return points;
 }
 
+/* calculates the total points a racer earned in all the races they 
+ * participated in */
 int calc_total_points(racer racer){
   int i, total_points = 0;
   for (i = 0; i < RACE_COUNT; i++)
@@ -321,9 +353,9 @@ int calc_total_points(racer racer){
   return total_points;
 }
 
-void print_italians_over30(){
+/* prints the results of all italian racers over 30 */
+void print_italians_over30(FILE *pinput_file){
   int i = 0, j;
-  FILE *pinput_file = fopen("cykelloeb", "r");
   int ita_lines = line_counter(pinput_file, "ITA");
   racer *result = (racer*) malloc(ita_lines * sizeof(racer));
   calc_italians_over30(pinput_file, result);
@@ -346,6 +378,7 @@ void print_italians_over30(){
   return;
 }
 
+/* finds the results of all italian racers over the age of 30 */
 void calc_italians_over30(FILE *pinput_file, racer result[]){
   int i, j = 0;
   int lines = line_counter(pinput_file, "\n");
@@ -362,10 +395,10 @@ void calc_italians_over30(FILE *pinput_file, racer result[]){
   return;
 }
 
-void print_finished_danes(){
+/* prints the results of all danish racers */
+void print_finished_danes(FILE *pinput_file){
   int i = 0;
   int finished_races;
-  FILE *pinput_file = fopen("cykelloeb", "r");
   int den_lines = line_counter(pinput_file, "DEN");
   racer *result = (racer*) malloc(den_lines * sizeof(racer));
   find_finished_danes(pinput_file, result);
@@ -384,6 +417,7 @@ void print_finished_danes(){
   return;
 }
 
+/* this function finds all the danish racers who finished one or more races */
 void find_finished_danes(FILE *pinput_file, racer result[]){
   int i, j = 0;
   int lines = line_counter(pinput_file, "\n");
@@ -400,6 +434,7 @@ void find_finished_danes(FILE *pinput_file, racer result[]){
   return;
 }
 
+/* finds out how many races a given racer finished (time or OTL) */
 int calc_finished_races(racer racer){
   int races = 0, i;
   for (i = 0; i < RACE_COUNT; i++){
@@ -411,9 +446,9 @@ int calc_finished_races(racer racer){
   return races;
 }
 
-void print_top10_finishers(){
+/* prints the top 10 racers, based on points in all races */
+void print_top10_finishers(FILE *pinput_file){
   int i = 0;
-  FILE *pinput_file = fopen("cykelloeb", "r");
   int top10_lines = line_counter(pinput_file, "\n");
   racer *result = (racer*) malloc(top10_lines * sizeof(racer));
   calc_top10_finishers(pinput_file, result);
@@ -430,6 +465,7 @@ void print_top10_finishers(){
   return;
 }
 
+/* finds and sorts the top 10 finishers by points and name */
 void calc_top10_finishers(FILE *pinput_file, racer result[]){
   int lines = line_counter(pinput_file, "\n");
   parsed_line *parsed_lines = (parsed_line*) malloc(lines*sizeof(parsed_line));
@@ -439,6 +475,8 @@ void calc_top10_finishers(FILE *pinput_file, racer result[]){
   return;
 }
 
+/* runs a simple compare on the points of racers, if two racers have equal
+ * points (comp = 0), the function compares by last name instead */
 int compare_points_and_name(const void *a, const void *b) {
   racer *ia = (racer *)a;
   racer *ib = (racer *)b;
@@ -453,23 +491,23 @@ int compare_points_and_name(const void *a, const void *b) {
   }
 }
 
-void print_paris_amstel_winner(){
+/* prints the racer(s) with the best total time in the paris and amstel races */
+void print_paris_amstel_winner(FILE *pinput_file){
   int i;
-  FILE *pinput_file = fopen("cykelloeb", "r");
   int lines = line_counter(pinput_file, "\n");
   char time[MAX_TIME];
   racer *result = (racer*) malloc(lines * sizeof(racer));
   int shared_firsts = calc_paris_amstel_winner(pinput_file, result, time);
   if (shared_firsts == 0)
   {
-  printf("The racer with the lowest combined time in the"
-      " Paris and Amstel races was: %s %s with a combined time of %s \n\n",
-      result[0].first_name, result[0].last_name, time);
+    printf("The racer with the lowest combined time in the"
+        " Paris and Amstel races was: %s %s with a combined time of %s \n\n",
+        result[0].first_name, result[0].last_name, time);
   }
   else{
-  printf("The racer(s) with the lowest combined time in the"
-      " Paris and Amstel races was: %s %s",
-      result[0].first_name, result[0].last_name);
+    printf("The racers with the lowest combined time in the"
+        " Paris and Amstel races was: %s %s",
+        result[0].first_name, result[0].last_name);
     for (i = 1; i < shared_firsts; i++){
       printf(", %s %s", result[i].first_name, result[i].last_name);
     }
@@ -478,11 +516,12 @@ void print_paris_amstel_winner(){
   }
 }
 
+/* calculates the racer with the lowest time */
 int calc_paris_amstel_winner(FILE *pinput_file, racer racers[], char time[]){
   int i, j = 0;
   int lines = line_counter(pinput_file, "\n");
   int *times = (int*) malloc(lines*sizeof(int));
-  int lowest_time = 10000000;
+  int lowest_time = INT_MAX;
   parsed_line *parsed_lines = (parsed_line*) malloc(lines*sizeof(parsed_line));
   racer *results = (racer*) malloc(lines * sizeof(racer));
   gen_racers(pinput_file, lines, parsed_lines, results);
@@ -509,6 +548,7 @@ int calc_paris_amstel_winner(FILE *pinput_file, racer racers[], char time[]){
   return j;
 }
 
+/* calculates the total time of a racer in seconds */
 int calc_total_time(char time1[], char time2[]){
   int hour1, min1, sec1, hour2, min2, sec2;
   sscanf(time1, " %d:%d:%d", &hour1, &min1, &sec1);
@@ -517,6 +557,7 @@ int calc_total_time(char time1[], char time2[]){
   return (sec1 + sec2) + ((min1 + min2) * 60) + ((hour1 + hour2) * 60 * 60);
 }
 
+/* reformats the time from seconds back into HH:MM:SS */
 void reformat_time(int seconds, char time[]){
   int hours, minutes, res;
 
@@ -528,28 +569,27 @@ void reformat_time(int seconds, char time[]){
   sprintf(time, "%d:%d:%d", hours, minutes, res);
 }
 
+/* compares two integers */
 int time_compare(const void *a, const void *b){ 
   const int *ia = (const int*)a;
   const int *ib = (const int*)b;
   return *ia - *ib;
 }
 
-void print_top10_avg_age(){
-  FILE *pinput_file = fopen("cykelloeb", "r");
+/* prints the average age of all top 10 finishers */
+void print_top10_avg_age(FILE *pinput_file){
   double average_age;
   average_age = calc_top10_avg_age(pinput_file);
   printf("The average age of all top 10 finishers is: %0.2f \n", average_age);
   return;
 }
-
+/* finds all racers who finished in the top 10 of a race, and calculates their 
+ * average age */
 double calc_top10_avg_age(FILE *pinput_file){
   int i, j;
   int lines = line_counter(pinput_file, "\n");
-  /*int top10_finish_lines = 10*4;*/
   double total_age = 0.0;
-  int unique_racers = 0;
-  /*racer *top10_racers = (racer*) malloc(top10_finish_lines * sizeof(racer));
-   */
+  double unique_racers = 0;
   racer *racers = (racer*) malloc(lines * sizeof(racer));
   parsed_line *parsed_lines = (parsed_line*) malloc(lines*sizeof(parsed_line));
   gen_racers(pinput_file, lines, parsed_lines, racers);
